4b7b04666fb38c431716330d5b3e4aa4e0aadfa9
Esron <esron.dtamar@gmail.com>
2022-08-02 18:39:54 -0300

    Add POST income route

--- src/main/java/dev/esron/IncomeResource.java
+++ src/main/java/dev/esron/IncomeResource.java
@@ -1,2 +1,23 @@
-package dev.esron;public class IncomeResource {
+package dev.esron;
+
+import io.smallrye.mutiny.Uni;
+
+import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/incomes")
+public class IncomeResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(@Valid Income income) {
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+
 }
