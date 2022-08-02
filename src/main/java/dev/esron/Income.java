d225557ab62dcad0481837865126a082e57d9818
Esron <esron.dtamar@gmail.com>
2022-08-02 18:39:06 -0300

    Add income entity with validations

--- src/main/java/dev/esron/Income.java
+++ src/main/java/dev/esron/Income.java
@@ -1,2 +1,19 @@
-package dev.esron;public class Income {
+package dev.esron;
+
+import javax.validation.constraints.*;
+
+public class Income {
+
+    @NotNull
+    @NotBlank
+    public String description;
+
+    @NotNull
+    @PositiveOrZero(message = "value must be zero or positive")
+    public Integer value;
+
+    @NotNull
+    @NotBlank
+    @Pattern(regexp = "([0-2][0-9]|3[0-1])\\/(0[1-9]|1[0-2])\\/[0-9]{4}", message = "invalid date format")
+    public String date;
 }
