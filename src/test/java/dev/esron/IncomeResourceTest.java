1ecf71e8e5dc8541e8a5e615d49047106940760f
Esron <esron.dtamar@gmail.com>
2022-08-04 19:17:05 -0300

    Test the response of a created income

--- src/test/java/dev/esron/IncomeResourceTest.java
+++ src/test/java/dev/esron/IncomeResourceTest.java
@@ -2,6 +2,7 @@ package dev.esron;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
+import org.jboss.resteasy.reactive.RestResponse.StatusCode;
 import org.junit.jupiter.api.Test;
 
 import javax.ws.rs.core.MediaType;
@@ -17,20 +18,28 @@ public class IncomeResourceTest {
     public void testCreateIncomeValidationTest() {
         given()
             .header("Content-Type", MediaType.APPLICATION_JSON)
-            .when().post("/incomes")
-            .then()
-                .statusCode(400);
-
-        given()
-            .header("Content-Type", MediaType.APPLICATION_JSON)
             .body("{\"description\": \"\", \"value\": \"\", \"date\": \"invalid\"}")
             .when().post("/incomes")
             .then()
-                .statusCode(400)
+                .statusCode(StatusCode.BAD_REQUEST)
                 .contentType(ContentType.JSON)
                 .body("title", equalTo("Constraint Violation"))
                 .body("violations.find { it.field == 'create.income.description' }.message", equalTo("must not be blank"))
                 .body("violations.find { it.field == 'create.income.value' }.message", equalTo("must not be null"))
                 .body("violations.find { it.field == 'create.income.date' }.message", equalTo("invalid date format"));
+    }
+
+    @Test
+    public void testCreateIncome() {
+        given()
+            .header("Content-Type", MediaType.APPLICATION_JSON)
+            .body("{\"description\": \"water bill\", \"value\": 2445, \"date\": \"02/08/2022\"}")
+            .when().post("/incomes")
+            .then()
+                .statusCode(StatusCode.CREATED)
+                .contentType(ContentType.JSON)
+                .body("description", equalTo("water bill"))
+                .body("value", equalTo(2445))
+                .body("date", equalTo("02/08/2022"));
     }
 }
