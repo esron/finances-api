87c87da76cf24cde5e60a5549ada90b80685db3d
Esron <esron.dtamar@gmail.com>
2022-08-09 20:03:13 -0300

    Change IncomeResource to a PanacheEntityResource

--- src/main/java/dev/esron/IncomeResource.java
+++ src/main/java/dev/esron/IncomeResource.java
@@ -1,23 +1,11 @@
 package dev.esron;
 
+import io.quarkus.hibernate.reactive.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.ResourceProperties;
 import io.smallrye.mutiny.Uni;
 
-import javax.validation.Valid;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 
-@Path("/incomes")
-public class IncomeResource {
-
-    @POST
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response create(@Valid Income income) {
-        return Response.status(Response.Status.BAD_REQUEST).build();
-    }
-
+@ResourceProperties(path = "incomes")
+public interface IncomeResource extends PanacheEntityResource<Income, Long> {
 }
